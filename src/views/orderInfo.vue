aecb3e879907db845eec7b1338181e85bb97c9a7
yinchengda <1061389225@qq.com>
2018-11-23 16:25:19 +0800

    emm....

--- src/views/orderInfo.vue
+++ src/views/orderInfo.vue
@@ -8,10 +8,52 @@
                 </span>
             </div>
             <div class="order-status-text">
-                待发货
+                待支付
+            </div>
+        </div>
+        <div class="courier-number">
+            <div class="icon-box">
+                <i class="iconfont icon-wuliu"></i>
+            </div>
+            <div class="text">
+                <p class="title-num">快递单号: 暂无快递单号</p>
+                <p class="info">暂无物流信息</p>
+            </div>
+        </div>
+        <div class="courier-number">
+            <div class="icon-box">
+                <i class="iconfont icon-dizhi1"></i>
+            </div>
+            <div class="text">
+                <p class="title-num">啊哈哈 16601119346</p>
+                <p class="info">八里桥小山沟</p>
             </div>
         </div>
 
+        <div class="order-shop-message">
+            <div class="title">
+                商品详情
+            </div>
+            <div class="shop-list">
+                <div class="item">
+                    <div class="img-box">
+                        <img src="" alt="" />
+                    </div>
+                    <div class="text">
+                        <div class="shop-price-title">
+                            <p class="shop-title">
+                            透气呼吸纱四件套【测试商品，不发货】
+                            </p>
+                            <span>￥0.01</span>
+                        </div>
+                        <div class="shop-drescribe">
+                            <span class="shop-type">测试规格:测试1</span>
+                            <span>×1</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
